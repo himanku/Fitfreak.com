dc2bd32e252c853069970186ef40963dfba970cc
Himanku Gogoi <himankugogoi5@gmail.com>
2022-11-13 17:50:20 +0530

    final changes done

--- fit-freak/src/Context/CartContext/reducer.js
+++ fit-freak/src/Context/CartContext/reducer.js
@@ -1,10 +1,32 @@
 // reducer related to cart state;
+import Swal from "sweetalert2";
+const alert = () => {
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Item added to cart',
+        showConfirmButton: false,
+        timer: 1500
+      })
+}
+
+const removeAlert = () => {
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Item removed from cart',
+        showConfirmButton: false,
+        timer: 1500
+      })
+}
 const reducer = (state,action) => {
     switch(action.type) {
         case "ADD_TO_CART" : {
+            alert();
             return [...state, action.payload]
         }
         case "REMOVE_FROM_CART" : {
+            removeAlert();
             const cartAfterRemoval = state.filter((item)=>
                 item.id!==action.payload
             )
